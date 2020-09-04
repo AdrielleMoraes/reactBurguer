cdb0945d0d3edce393cfb0614f6091e8a65da03f
adriellemoraes <adrielle.nazar@hotmail.com>
2020-09-04 20:11:55 +0100

    delete person

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -2,86 +2,90 @@ import React, { Component } from 'react';
 import './App.css';
 // import my component file 
 import Person from "./Person/Person";
+import person from './Person/Person';
 
 class App extends Component{
 
-  state = {
-    persons:[
-      {name:"Adrielle", age: 23},
-      {name:"Max", age: 32},
-      {name:"Andy", age: 25}
-    ],
-    showPersons: false
-  }
-
-
-  switchNameHandler = (factor)=>
-  {
-    let temp_persons = this.state.persons.sort(() => Math.random() - factor);
-
-    //update props
-    this.setState({
-      persons: temp_persons
-    });
-  };
-
-  togglePersonsHandler = () =>{
-
-    this.setState({
-      showPersons: !this.state.showPersons
-    })
-  }
-
-  nameChangedHandler = event =>{
-    this.setState({
+   state = {
       persons:[
-        {name: event.target.value, age: 23},
-        {name:"Max", age: 32},
-        {name:"Andy", age: 25}
-      ]
-    });
-  }
+         {id: 'dsada', name:"Adrielle", age: 23},
+         {id: 'sdasd', name:"Max", age: 32},
+         {id: 'ererr', name:"Andy", age: 25}
+      ],
+      showPersons: true
+   }
 
-  render (){
-    const style ={
-      color: "Black"
-    }
+   deletePersonHandler = (personIndex)=>{
+      // fetch all persons from array
+		const persons = [...this.state.persons]
+      // remove element form array
+		persons.splice(personIndex,1);
+		
+		//update state
+      this.setState({persons: persons});
+   }
+   
+   togglePersonsHandler = () =>{
+
+      this.setState({
+         showPersons: !this.state.showPersons
+      })
+   }
+
+   nameChangedHandler = (event, id) =>{
+
+		//find index of the object given an arrow function as the criteria for selecting it
+		const personIndex = this.state.persons.findIndex(p =>{
+			return p.id === id;
+		});
+
+		//copy person to a new variable
+		const person = {...this.state.persons[personIndex]};
+
+		// apply event
+		person.name = event.target.value;
+
+		//copy all persons 
+		const persons = [...this.state.persons];
+
+		// assign updated person to persons list
+		persons[personIndex] = person;
+
+		//update state
+      this.setState({ persons:persons });
+   }
+
+   render (){
+      const style ={
+         color: "Black"
+      }
 
 
-  let persons = null
-  if (this.state.showPersons){
-    persons = (
-      <div  >
-        <Person 
-          name = {this.state.persons[0].name} 
-          age = {this.state.persons[0].age}
-          click = {this.switchNameHandler.bind(this, 1)}
-          changed = {this.nameChangedHandler}
-        />
-        <Person 
-          name = {this.state.persons[1].name} 
-          age = {this.state.persons[1].age}
-          click = {this.switchNameHandler.bind(this, 0.5)}
-        >My hobbies: Racing </Person>
-        <Person 
-          name = {this.state.persons[2].name} 
-          age = {this.state.persons[2].age}
-        />
-      </div>
-    );
-  }
+   let persons = null
+   if (this.state.showPersons){
+      persons = (
+         <div  >
+            {this.state.persons.map((person, index) =>{
+					return <Person 
+									changed = {(event)=>this.nameChangedHandler(event, person.id)}
+                           click ={()=>this.deletePersonHandler(index)}
+                           name = {person.name}
+									age = {person.age}
+									key = {person.id}
+                     />
+            })}
+         </div>
+      );	
+   }
 
-  return(
-    <div className="App">
-      <h1>Hello, i am a react app</h1>
-      <button style={style} onClick={()=> this.togglePersonsHandler()}>Toggle name</button>
+   return(
+		<div className="App">
+			<h1>Hello, i am a react app</h1>
+			<button style={style} onClick={()=> this.togglePersonsHandler()}>Toggle name</button>
+			{persons}
 
-      {persons}
-      
-    </div>
-  )};
-
-  //return React.createElement("div", {className: "App"}, React.createElement("h1", null,"Hi, I\'m a react app"));
+		</div>
+   )}
 }
 
 export default App;
--- helloworld/src/Person/Person.js
+++ helloworld/src/Person/Person.js
@@ -6,10 +6,11 @@ import './Person.css';
 // create a person component
 const person = (props) =>{
     //let age = Math.floor(Math.random()*30)
-    return  <div className="Person">
-                <p onClick= {props.click}> I'm {props.name} and I am  years {props.age} old</p>
+    return  <div  className="Person">
+                <p >I'm {props.name} and I am  years {props.age} old</p>
                 <p>{props.children}</p>
                 <input type="text" onChange={props.changed} value={props.name}></input>
+                <p onClick= {props.click}>delete</p>
             </div>
 }
 
