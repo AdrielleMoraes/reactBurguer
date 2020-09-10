2c89a096b327fad4ebc35daaf64019d1295291fb
adriellemoraes <adrielle.nazar@hotmail.com>
2020-09-10 22:49:22 +0100

    started

---
+++ src/Person/Person.js
@@ -0,0 +1,38 @@
+// import react 
+import React from "react";
+import Radium from "radium";
+import './Person.css';
+
+//css 
+import styled from "styled-components";
+
+
+const StyledDiv = styled.div`
+    width: 30%;
+    margin:16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+    @media (min-width: 500px){
+        width: 450px;
+    }      
+`;
+
+// create a person component
+const person = (props) =>{
+ 
+    //let age = Math.floor(Math.random()*30)
+    return(  
+    // <div className="Person">
+    <StyledDiv>
+        <p >I'm {props.name} and I am  years {props.age} old</p>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}></input>
+        <p onClick= {props.click}>delete</p>
+    </StyledDiv>);
+}
+
+
+export default Radium(person);
