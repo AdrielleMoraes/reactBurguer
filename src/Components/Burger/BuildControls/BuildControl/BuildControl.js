7ec8f5ce2d76752787c9955ad33632dd1b13fe8e
Adrielle <adrielle.nazar@hotmail.com>
2021-01-26 14:00:55 -0300

    Adding buttons

--- src/Components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/Components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,7 +2,13 @@ import React from 'react'
 import classes from "./BuildControl.css"
 
 const BuildControl = (props) => {
-    return(<div/>)
+    return(
+        <div className={classes.BuildControl}>
+            <label className={classes.Label}>{props.label}</label>
+            <button className={classes.Less}>-</button>
+            <button className={classes.More}>+</button>
+        </div>
+    )
 }
 
 export default BuildControl
--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,23 @@
 import React from 'react'
-import classes from "./BuildControl.css"
+import classes from "./BuildControls.css"
+import BuildControl from "./BuildControl/BuildControl"
 
+const controls=[
+    {label:"Salad", type:"salad"},
+    {label:"Bacon", type:"bacon"},
+    {label:"Cheese", type:"cheese"},
+    {label:"Meat", type:"meat"}
+]
 const BuildControls = (props) => {
-    return(<div/>)
+
+
+    return(
+        <div className={classes.BuildControls}>
+            {controls.map(ingredient=>{
+                return <BuildControl key={ingredient.type} label={ingredient.label} />
+            })}
+        </div>
+    )
 }
 
 export default BuildControls
