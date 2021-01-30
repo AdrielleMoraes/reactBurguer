7806afcd508f843a2513071d55c5f88e97f6f6ce
Adrielle <adrielle.nazar@hotmail.com>
2021-01-30 15:45:14 -0300

    new backdrop component

--- src/Components/UI/Backdrop/Backdrop.js
+++ src/Components/UI/Backdrop/Backdrop.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import classes from "./BackDrop.css"
+
 const BackDrop = (props) => {
-    props.show?<div></div>:null
+    return (props.show?<div className={classes.BackDrop}></div>:null)
 }
 
 export default BackDrop
--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import classes from "./Modal.css"
+import Auxiliar from "../../../hoc/Auxiliar"
+import BackDrop from "../Backdrop/Backdrop"
 
 const Modal = (props) => {
     return(
-        <div style={{transform: props.show? "translateY(0)": "translateY(-100)",
-                     opacity: props.show? "1":"0"}} className={classes.Modal}>
-            {props.children}
-        </div>
+        <Auxiliar>
+            <BackDrop></BackDrop>
+            <div style={{transform: props.show? "translateY(0)": "translateY(-100)",
+                        opacity: props.show? "1":"0"}} className={classes.Modal}>
+                {props.children}
+            </div>
+        </Auxiliar>
     )
 }
 
